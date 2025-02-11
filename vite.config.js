7df543beba465033121fcb4014bb17e974693cd3
Yamashita <yamasyou199823@icloud.com>
2025-02-11 23:45:54 +0900

    Update package configuration and vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_PAGES  // この行を追加
-  ? "reactailwind"            // この行を追加
-  : "./",                     // この行を追加
+  base: '/dj-organizer/', // GitHub Pages の公開URLに合わせる
   plugins: [react()],
-})
+});
